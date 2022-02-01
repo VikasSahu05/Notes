5c79856c2680d38a06794df4fb721a28addeb647
Vikas Kumar Sahu <58459056+vikku002@users.noreply.github.com>
2022-02-01 13:58:34 +0530

    Add title text

--- script.js
+++ script.js
@@ -4,10 +4,11 @@ let addBtn = document.getElementById('addbtn');
 addBtn.addEventListener('click',function(e){
     e.preventDefault();
     let addTxt = document.getElementById('addtxt');
+    let title = document.getElementById('title');
     let txt = document.getElementById('addtxt').value;
     let notes =  localStorage.getItem('notes');
     if(txt == ""){
-        alert('Name sajcbjs');
+        alert('Enter text');
         return false;
     }
     if(notes == null ){
@@ -15,8 +16,13 @@ addBtn.addEventListener('click',function(e){
     }else{
         noteObj = JSON.parse(notes);
     }
-    noteObj.push(addTxt.value);
+    let myObj = {
+        title:title.value,
+        text:addTxt.value
+    }
+    noteObj.push(myObj);
     localStorage.setItem("notes",JSON.stringify(noteObj));
+    title.value="";
     addTxt.value="";
     // console.log(noteObj);
     showNotes();
@@ -34,8 +40,8 @@ function showNotes(){
     noteObj.forEach(function(element,index){
         html+=`<div class="noteCard my-2 mx-2 card" style="width: 18rem;">
         <div class="card-body">
-          <h5 class="card-title">Note ${index+1}</h5>
-          <p>${element}</p>
+          <h5 class="card-title">${element.title}</h5>
+          <p>${element.text}</p>
           <button id="${index}" onclick="deleteNote(this.id)" type="button" class="btn btn-primary">Delete Note</button>
         </div>
       </div>`;
